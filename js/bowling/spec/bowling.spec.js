0ae852387687b9a2059deb83bbd25b3e7bff66a9
Sebastian Springer <sebastian.springer@mayflower.de>
2013-03-13 08:03:28 +0100

    Third test - how many remaining frames?

--- js/bowling/spec/bowling.spec.js
+++ js/bowling/spec/bowling.spec.js
@@ -5,4 +5,19 @@ describe("Bowling Game", function () {
 
         expect(actual).toEqual(2);
     });
+
+    it("should create a second frame with 2 and 5 and return 7", function() {
+        var bowling = new BowlingGame();
+        var actual = bowling.createFrame(2,5);
+
+        expect(actual).toEqual(7);
+    });
+
+    it("should return 9, if questioned for remaining frames if one frame is played already", function () {
+        var bowling = new BowlingGame();
+        bowling.frames = 1;
+
+        var actual = bowling.remainginFrames();
+        expect(actual).toEqual(9);
+    });
 });
