1731ef410459c6c8129c7b9b90d61eb49c9c9a9d
Sebastian Springer <sebastian.springer@mayflower.de>
2013-03-13 08:09:53 +0100

    Refactoring of existing tests - use beforeEach to initialize bg

--- js/bowling/spec/bowling.spec.js
+++ js/bowling/spec/bowling.spec.js
@@ -1,20 +1,24 @@
 describe("Bowling Game", function () {
+
+    var bowling;
+
+    beforeEach(function () {
+        bowling = new BowlingGame();
+    });
+
     it("should create the first frame with 1 and 1 pin and return 2", function () {
-        var bowling = new BowlingGame();
         var actual = bowling.createFrame(1, 1);
 
         expect(actual).toEqual(2);
     });
 
     it("should create a second frame with 2 and 5 and return 7", function() {
-        var bowling = new BowlingGame();
         var actual = bowling.createFrame(2,5);
 
         expect(actual).toEqual(7);
     });
 
     it("should return 9, if questioned for remaining frames if one frame is played already", function () {
-        var bowling = new BowlingGame();
         bowling.frames = 1;
 
         var actual = bowling.remainingFrames();
@@ -22,7 +26,6 @@ describe("Bowling Game", function () {
     });
 
     it("should return 8, if questioned for remaining frames if two frames are played already", function () {
-        var bowling = new BowlingGame();
         bowling.frames = 2;
 
         var actual = bowling.remainingFrames();
