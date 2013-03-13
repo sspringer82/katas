6ec822ca571956b8aa830f407a2595010abc8b21
Sebastian Springer <sebastian.springer@mayflower.de>
2013-03-13 08:53:06 +0100

    Refactoring: added var result for calculating result

--- js/bowling/src/bowling.js
+++ js/bowling/src/bowling.js
@@ -4,13 +4,17 @@ BowlingGame = function() {
 };
 
 BowlingGame.prototype.createFrame = function(pins1, pins2) {
+    var result;
     this.frames += 1;
 
     if (pins1 === "X") {
-        return 10;
+        result = 10;
+    } else {
+        result = pins1 + pins2;
     }
-    this.results = [ { pin1:pins1, pin2:pins2, result:pins1 + pins2 } ];
-    return pins1 + pins2;
+
+    this.results = [ { pin1:pins1, pin2:pins2, result: result } ];
+    return result;
 };
 
 BowlingGame.prototype.remainingFrames = function () {
